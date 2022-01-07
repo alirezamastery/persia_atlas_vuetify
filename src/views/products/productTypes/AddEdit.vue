15184dbdbc288b925f9397d43dfec39ac71e84c8
alireza <alireza.mastery@gmail.com>
2022-01-07 16:30:29 +0330

    added productTypes list and add/edit views

--- src/views/products/productTypes/AddEdit.vue
+++ src/views/products/productTypes/AddEdit.vue
@@ -1,10 +1,124 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-card-title>
+      {{ formTitle }}
+    </v-card-title>
+
+    <v-card-text v-if="showForm">
+      <validation-observer
+          ref="form"
+          v-slot="{ invalid }"
+      >
+        <v-form @submit.prevent="saveItem">
+          <v-container>
+            <v-row>
+              <v-col
+                  cols="12"
+                  lg="4"
+                  sm="12"
+                  md="12"
+              >
+                <ValidationProvider name="Title" rules="required" v-slot="{ errors }">
+                  <v-text-field
+                      v-model="form.title"
+                      :label="$t('general.title')"
+                      :error-messages="errors"
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                  cols="12"
+                  lg="4"
+                  sm="12"
+                  md="12"
+              >
+                <ValidationProvider name="Product" rules="required" v-slot="{ errors }">
+                  <AutoComplete
+                      v-model="form.selectors"
+                      :label="$t('products.productType')"
+                      :query-param="'title'"
+                      :obj-repr-field="'title'"
+                      :api="$api.productTypeSelectors"
+                      :error-messages="errors"
+                      select-multiple
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+
+          </v-container>
+
+          <DetailViewActions
+              :save-disabled="invalid"
+              :show-delete="!!editingItemId"
+              v-on:delete="deleteDialog = true"
+          />
+
+        </v-form>
+      </validation-observer>
+    </v-card-text>
+
+    <DetailViewDeleteDialog
+        v-if="editingItemId"
+        v-model="deleteDialog"
+        :item-title="itemRepr"
+        v-on:delete="deleteItem"
+    />
+
+  </v-card>
 </template>
 
 <script>
+import {AddEditViewMixin} from '@/mixins/addEditView'
+import {textToolsMixin} from '@/mixins/textTools'
+import AutoComplete from '@/components/AutoComplete'
+import DetailViewActions from '@/components/general/DetailViewActions'
+import DetailViewDeleteDialog from '@/components/general/DetailViewDeleteDialog'
+
 export default {
-  name: 'AddEdit',
+  name: 'Details',
+  components: {
+    AutoComplete,
+    DetailViewActions,
+    DetailViewDeleteDialog,
+  },
+  mixins: [textToolsMixin, AddEditViewMixin],
+  props: {
+    id: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+  data() {
+    return {
+      apiRoot: this.$api.productTypes,
+      listViewRoute: 'productTypeList',
+      itemType: this.$t('products.productType'),
+      form: {
+        title: '',
+        selectors: [],
+      },
+    }
+  },
+  computed: {
+    itemRepr() {
+      return this.form.title.toString()
+    },
+  },
+  methods: {
+    formInit(resData) {
+      this.form = resData
+      this.form.selectors = resData.selectors.map(itm => itm.id)
+    },
+    getRequestData() {
+      return {
+        title: this.form.title,
+        selectors: this.form.selectors,
+      }
+    },
+  },
 }
 </script>
 
