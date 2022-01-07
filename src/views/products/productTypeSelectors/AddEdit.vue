5e86a57fca34e0feeaf677f585175b19aae57884
alireza <alireza.mastery@gmail.com>
2022-01-07 16:40:09 +0330

    added productTypeSelector list and add/edit views

--- src/views/products/productTypeSelectors/AddEdit.vue
+++ src/views/products/productTypeSelectors/AddEdit.vue
@@ -1,10 +1,98 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-card-title>
+      {{ formTitle }}
+    </v-card-title>
+
+    <v-card-text v-if="showForm">
+      <validation-observer
+          ref="form"
+          v-slot="{ invalid }"
+      >
+        <v-form @submit.prevent="saveItem">
+          <v-container>
+            <v-row>
+              <v-col
+                  cols="12"
+                  lg="4"
+                  sm="12"
+                  md="12"
+              >
+                <ValidationProvider name="Title" rules="required" v-slot="{ errors }">
+                  <v-text-field
+                      v-model="form.title"
+                      :label="$t('general.title')"
+                      :error-messages="errors"
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <DetailViewActions
+              :save-disabled="invalid"
+              :show-delete="!!editingItemId"
+              v-on:delete="deleteDialog = true"
+          />
+
+        </v-form>
+      </validation-observer>
+    </v-card-text>
+
+    <DetailViewDeleteDialog
+        v-if="editingItemId"
+        v-model="deleteDialog"
+        :item-title="itemRepr"
+        v-on:delete="deleteItem"
+    />
+
+  </v-card>
 </template>
 
 <script>
+import {AddEditViewMixin} from '@/mixins/addEditView'
+import {textToolsMixin} from '@/mixins/textTools'
+import DetailViewActions from '@/components/general/DetailViewActions'
+import DetailViewDeleteDialog from '@/components/general/DetailViewDeleteDialog'
+
 export default {
-  name: 'AddEdit',
+  name: 'Details',
+  components: {
+    DetailViewActions,
+    DetailViewDeleteDialog,
+  },
+  mixins: [textToolsMixin, AddEditViewMixin],
+  props: {
+    id: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+  data() {
+    return {
+      apiRoot: this.$api.productTypeSelectors,
+      listViewRoute: 'productTypeSelectorList',
+      itemType: this.$t('products.productTypeSelector'),
+      form: {
+        title: '',
+      },
+    }
+  },
+  computed: {
+    itemRepr() {
+      return this.form.title.toString()
+    },
+  },
+  methods: {
+    formInit(resData) {
+      this.form = resData
+    },
+    getRequestData() {
+      return {
+        title: this.form.title,
+      }
+    },
+  },
 }
 </script>
 
--- src/views/products/productTypeSelectors/List.vue
+++ src/views/products/productTypeSelectors/List.vue
@@ -1,10 +1,80 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-card-title>
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+          :headers="headers"
+          :items="items"
+          :items-per-page="100"
+          item-key="id"
+          class="elevation-1"
+          dense
+          multi-sort
+      >
+        <!-- Customize table header START -->
+        <template v-slot:top>
+          <ListViewTableHeader
+              :api-root="apiRoot"
+              :title="$t('general.routes.productTypeSelectors')"
+              :add-route="addRoute"
+              v-on:search-result="items = $event"
+              :show-search="false"
+          />
+        </template>
+        <!-- Customize table header END -->
+
+        <!-- Customize how each row is displayed START -->
+        <template v-slot:item.title="{ item }">
+          <v-btn text :to="{name: editRoute, params:{id:item.id}}" class="text-none">
+            {{ item.title }}
+          </v-btn>
+        </template>
+
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+              small
+              @click="$router.push({name: editRoute, params: {id: item.id}})"
+          >
+            mdi-pencil
+          </v-icon>
+        </template>
+        <!-- Customize how each row is displayed END -->
+
+        <template v-slot:no-data>
+          {{ $t('general.noItemsFound') }}
+        </template>
+
+      </v-data-table>
+
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import {listViewMixin} from '@/modules/mixins'
+import ListViewTableHeader from '@/components/general/ListViewTableHeader'
+
 export default {
   name: 'List',
+  components: {
+    ListViewTableHeader,
+  },
+  mixins: [listViewMixin],
+  data() {
+    return {
+      apiRoot: this.$api.productTypeSelectors,
+      editRoute: 'productTypeSelectorEdit',
+      addRoute: 'productTypeSelectorAdd',
+      headers: [
+        {text: this.$t('general.title'), value: 'title', sortable: false},
+        {text: this.$t('products.tools'), value: 'actions', sortable: false},
+      ],
+      editedItem: {
+        title: '',
+      },
+    }
+  },
 }
 </script>
 
