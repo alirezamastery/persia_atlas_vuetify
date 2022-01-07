3d02f51ae982e52784dc278b6f9a08e64ab42be2
alireza <alireza.mastery@gmail.com>
2022-01-07 15:44:09 +0330

    added products list and add/edit views

--- src/views/products/products/AddEdit.vue
+++ src/views/products/products/AddEdit.vue
@@ -1,10 +1,151 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-card-title>
+      {{ formTitle }}
+    </v-card-title>
+
+    <v-card-text v-if="showForm">
+      <validation-observer
+          ref="form"
+          v-slot="{ invalid }"
+      >
+        <v-form @submit.prevent="saveItem">
+          <v-container>
+            <v-row>
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <ValidationProvider name="dkp" rules="required" v-slot="{ errors }">
+                  <v-text-field
+                      v-model="form.dkp"
+                      :label="$t('products.dkp')"
+                      :error-messages="errors"
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <ValidationProvider name="Title" rules="required" v-slot="{ errors }">
+                  <v-text-field
+                      v-model="form.title"
+                      :label="$t('general.title')"
+                      :error-messages="errors"
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <v-checkbox
+                    v-model="form.is_active"
+                    :label="$t('products.isActive')"
+                />
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col
+                  cols="12"
+                  lg="4"
+                  sm="12"
+                  md="12"
+              >
+                <ValidationProvider name="SelectorValues" rules="required" v-slot="{ errors }">
+                  <AutoComplete
+                      v-model="form.type.id"
+                      :label="$t('products.productType')"
+                      :query-param="'search'"
+                      :obj-repr-field="'title'"
+                      :api="$api.productTypes"
+                      :error-messages="errors"
+                  />
+                </ValidationProvider>
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <DetailViewActions
+              :save-disabled="invalid"
+              :show-delete="!!editingItemId"
+              v-on:delete="deleteDialog = true"
+          />
+
+        </v-form>
+      </validation-observer>
+    </v-card-text>
+
+    <DetailViewDeleteDialog
+        v-if="editingItemId"
+        v-model="deleteDialog"
+        :item-title="itemRepr"
+        v-on:delete="deleteItem"
+    />
+
+  </v-card>
 </template>
 
 <script>
+import {AddEditViewMixin} from '@/mixins/addEditView'
+import {textToolsMixin} from '@/mixins/textTools'
+import DetailViewActions from '@/components/general/DetailViewActions'
+import DetailViewDeleteDialog from '@/components/general/DetailViewDeleteDialog'
+import AutoComplete from '@/components/AutoComplete'
+
 export default {
-  name: 'AddEdit',
+  name: 'Details',
+  components: {
+    DetailViewActions,
+    DetailViewDeleteDialog,
+    AutoComplete,
+  },
+  mixins: [textToolsMixin, AddEditViewMixin],
+  props: {
+    id: {
+      type: [Number, String],
+      default: null,
+    },
+  },
+  data() {
+    return {
+      apiRoot: this.$api.products,
+      listViewRoute: 'productList',
+      itemType: this.$t('products.product'),
+      form: {
+        title: '',
+        dkp: null,
+        is_active: true,
+        type: {},
+      },
+    }
+  },
+  computed: {
+    itemRepr() {
+      return this.form.title.toString()
+    },
+  },
+  methods: {
+    formInit(resData) {
+      this.form = resData
+    },
+    getRequestData() {
+      return {
+        title: this.form.title,
+        dkp: this.form.dkp,
+        is_active: this.form.is_active,
+        type: this.form.type.id,
+      }
+    },
+  },
 }
 </script>
 
--- src/views/products/products/List.vue
+++ src/views/products/products/List.vue
@@ -1,10 +1,111 @@
 <template>
-  <div></div>
+  <v-card flat>
+    <v-card-title>
+    </v-card-title>
+    <v-card-text>
+      <v-data-table
+          :headers="headers"
+          :items="items"
+          :items-per-page="100"
+          item-key="id"
+          class="elevation-1"
+          dense
+          multi-sort
+      >
+        <!-- Customize table header START -->
+        <template v-slot:top>
+          <ListViewTableHeader
+              :title="$t('general.routes.products')"
+              :api-root="apiRoot"
+              :add-route="'productAdd'"
+              v-on:search-result="items = $event"
+          />
+        </template>
+        <!-- Customize table header END -->
+
+        <!-- Customize how each row is displayed START -->
+        <template v-slot:item.title="{ item }">
+          <v-btn text :to="{name: editRoute, params: {id: item.id}}">
+            {{ item.title }}
+          </v-btn>
+        </template>
+
+        <template v-slot:item.dkp="{ item }">
+          {{ item.dkp }}
+        </template>
+
+        <template v-slot:item.is_active="{ item }">
+          <v-icon
+              v-if="item.is_active"
+              right
+              small
+              color="green"
+          >
+            mdi-checkbox-marked-circle
+          </v-icon>
+          <v-icon
+              v-else
+              right
+              small
+              color="red"
+          >
+            mdi-cancel
+          </v-icon>
+        </template>
+
+        <template v-slot:item.type="{ item }">
+          {{ item.type.title | price }}
+        </template>
+
+        <template v-slot:item.actions="{ item }">
+          <v-icon
+              small
+              @click="$router.push({name: editRoute, params: {id: item.id}})"
+          >
+            mdi-pencil
+          </v-icon>
+        </template>
+        <!-- Customize how each row is displayed END -->
+
+        <template v-slot:no-data>
+          {{ $t('general.noItemsFound') }}
+        </template>
+
+      </v-data-table>
+
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
+import {listViewMixin} from '@/modules/mixins'
+import ListViewTableHeader from '@/components/general/ListViewTableHeader'
+
 export default {
   name: 'List',
+  components: {
+    ListViewTableHeader,
+  },
+  mixins: [listViewMixin],
+  data() {
+    return {
+      apiRoot: this.$api.products,
+      editRoute: 'productEdit',
+      headers: [
+        {text: this.$t('general.title'), value: 'title', sortable: false},
+        {text: this.$t('general.dkp'), value: 'dkp'},
+        {text: this.$t('general.isActive'), value: 'is_active'},
+        {text: this.$t('general.type'), value: 'type', sortable: false},
+        {text: this.$t('products.tools'), value: 'actions', sortable: false},
+      ],
+      editedItem: {
+        title: null,
+        dkp: null,
+        is_active: null,
+        type: null,
+      },
+    }
+  },
 }
 </script>
 
