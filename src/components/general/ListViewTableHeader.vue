e6da287bdf7b0547fb18a34ca9e88f8b46d46d38
alireza <alireza.mastery@gmail.com>
2022-01-12 20:42:47 +0330

    fixed debounce

--- src/components/general/ListViewTableHeader.vue
+++ src/components/general/ListViewTableHeader.vue
@@ -72,28 +72,9 @@ export default {
     }
   },
   methods: {
-    // TODO: fix debounce
-    handleSearchInput() {
+    handleSearchInput: debounce(function () {
       this.$emit('search-input', this.searchPhrase)
-      // const apiCall = (url) => {
-      //   this.axios.get(url)
-      //       .then(res => {
-      //         console.log('ListViewTableHeader | handleSearchInput', res)
-      //         this.$emit('search-result', res.data.items)
-      //       })
-      //       .catch(err => {
-      //         console.log(err)
-      //       })
-      // }
-      //
-      // const debouncedAPICall = debounce(apiCall, 300)
-      //
-      // this.loading = true
-      // let url = this.apiRoot
-      // if (this.searchPhrase !== '')
-      //   url += `?${this.queryParam}=${this.searchPhrase}`
-      // debouncedAPICall(url)
-    },
+    }, 300),
   },
 }
 </script>
--- src/modules/api-tools.js
+++ src/modules/api-tools.js
@@ -1,4 +1,4 @@
-export function debounce(func, delay) {
+export function debounce_1(func, delay) {
   let timeout
   return (...args) => {
     if (timeout) {
@@ -7,6 +7,18 @@ export function debounce(func, delay) {
     const context = this //important, without context the input url to apiCall will be undefined
     timeout = setTimeout(() => {
       func.apply(context, args)
+    }, delay)
+  }
+}
+
+export function debounce(fn, delay) {
+  let timeoutID = null
+  return function () {
+    clearTimeout(timeoutID)
+    const args = arguments
+    const that = this
+    timeoutID = setTimeout(function () {
+      fn.apply(that, args)
     }, delay)
   }
 }
