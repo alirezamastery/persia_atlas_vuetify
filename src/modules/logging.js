f80155cbc5efd27f23386bed6967db11c7001892
alireza <alireza.mastery@gmail.com>
2022-01-16 20:03:46 +0330

    minor changes

--- src/modules/logging.js
+++ src/modules/logging.js
@@ -2,4 +2,15 @@ export const logger = (text, color = 'white') => {
   console.log(`%c${text}`, `color: ${color};`)
 }
 
+// export function logger() {
+//   console.log(arguments)
+//   let color = 'red'
+//   let txt = ''
+//   for (let i = 0; i < arguments.length - 1; i++) {
+//     txt += JSON.stringify(arguments[i]) + ' '
+//   }
+//
+//   console.log(`%c${txt}`, `color: ${color};`)
+// }
+
 export default logger
