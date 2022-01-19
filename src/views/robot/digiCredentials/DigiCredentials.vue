265ede758d109dbb6ed56cbb778533d635453cb8
alireza <alireza.mastery@gmail.com>
2022-01-19 22:40:43 +0330

    code refactoring

--- src/views/robot/digiCredentials/DigiCredentials.vue
+++ src/views/robot/digiCredentials/DigiCredentials.vue
@@ -5,9 +5,9 @@
       <v-form @submit.prevent="handleSubmit">
         <v-container>
           <v-row>
-            <v-col cols="12" sm="8" md="5" lg="3">
+            <v-col cols="10" sm="8" md="5" lg="3">
               <v-text-field
-                  v-model="username"
+                  v-model="newUsername"
                   :disabled="!editUsername"
                   :label="$t('general.username')"
                   class="latin-text-font"
@@ -24,7 +24,7 @@
           <v-row>
             <v-col cols="10" sm="8" md="5" lg="3">
               <v-text-field
-                  v-model="password"
+                  v-model="newPassword"
                   :disabled="!editPassword"
                   :label="$t('general.password')"
                   class="latin-text-font"
@@ -39,7 +39,14 @@
             </v-col>
           </v-row>
         </v-container>
-        <v-btn type="submit" color="green">{{ $t('general.submit') }}</v-btn>
+        <v-btn
+            type="submit"
+            color="orange"
+            :disabled="!hasChange"
+            style="color: black"
+        >
+          {{ $t('general.submit') }}
+        </v-btn>
       </v-form>
     </v-card-text>
   </v-card>
@@ -50,29 +57,43 @@ export default {
   name: 'DigiCredentials',
   data() {
     return {
-      username: '',
-      password: '',
+      oldUsername: '',
+      oldPassword: '',
       editUsername: false,
       editPassword: false,
+      newUsername: '',
+      newPassword: '',
     }
+  },
+  computed: {
+    hasChange() {
+      return this.newUsername !== this.oldUsername || this.newPassword !== this.oldPassword
+    },
   },
   created() {
     this.axios.get(this.$api.digiCreds)
         .then(res => {
-          this.username = res.data.digi_username
-          this.password = res.data.digi_password
+          this.oldUsername = res.data.digi_username
+          this.newUsername = this.oldUsername
+          this.oldPassword = res.data.digi_password
+          this.newPassword = this.oldPassword
         })
   },
   methods: {
     handleSubmit() {
       const data = {
-        'digi_username': this.username,
-        'digi_password': this.password,
+        'digi_username': this.newUsername,
+        'digi_password': this.newPassword,
       }
       this.axios.post(this.$api.digiCreds, data)
           .then(res => {
             console.log('digi password', res)
+            this.editUsername = false
             this.editPassword = false
+            this.oldUsername = res.data.digi_username
+            this.newUsername = this.oldUsername
+            this.oldPassword = res.data.digi_password
+            this.newPassword = this.oldPassword
             this.addSnackbar('green', this.$t('general.snack.saveSuccess'))
           })
           .catch(err => {
